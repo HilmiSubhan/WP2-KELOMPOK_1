d363225de50058f32326ffac2b2415c2e599ae74
AmarantiDeriza <133253713+AmarantiDeriza@users.noreply.github.com>
2023-09-24 17:29:25 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1><center>Perkenalkan</center></h1>";
+        echo"<p><center>Nama saya Amaranti Satya Deriza nan baik hati</center></p>";
+        echo "<p><center>Salam Kenal</center></p>";
+    }
+    
+}
